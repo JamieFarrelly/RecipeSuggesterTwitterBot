4b3c7fd74cbf1752e29c7fd1e4b3b65a0d055015
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-03-05 20:40:40 +0000

    formatting fixed

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -3,24 +3,27 @@ public class Recipe {
 
 	private String recipeUrl;
 	private String recipeName;
-	
+
 	public Recipe(String recipeUrl, String recipeName) {
 		super();
 		this.recipeUrl = recipeUrl;
 		this.recipeName = recipeName;
 	}
-	
+
 	public String getRecipeUrl() {
 		return recipeUrl;
 	}
+
 	public void setRecipeUrl(String recipeUrl) {
 		this.recipeUrl = recipeUrl;
 	}
+
 	public String getRecipeName() {
 		return recipeName;
 	}
+
 	public void setRecipeName(String recipeName) {
 		this.recipeName = recipeName;
 	}
-	
+
 }
