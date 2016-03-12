c273b481b1ccc8c1e38e15b8b913bc460e7b8dd9
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-03-12 19:15:33 +0000

    Building jar with dependencies and formatting with spaces

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,29 +1,29 @@
 
 public class Recipe {
 
-	private String recipeUrl;
-	private String recipeName;
+    private String recipeUrl;
+    private String recipeName;
 
-	public Recipe(String recipeUrl, String recipeName) {
-		super();
-		this.recipeUrl = recipeUrl;
-		this.recipeName = recipeName;
-	}
+    public Recipe(String recipeUrl, String recipeName) {
+        super();
+        this.recipeUrl = recipeUrl;
+        this.recipeName = recipeName;
+    }
 
-	public String getRecipeUrl() {
-		return recipeUrl;
-	}
+    public String getRecipeUrl() {
+        return recipeUrl;
+    }
 
-	public void setRecipeUrl(String recipeUrl) {
-		this.recipeUrl = recipeUrl;
-	}
+    public void setRecipeUrl(String recipeUrl) {
+        this.recipeUrl = recipeUrl;
+    }
 
-	public String getRecipeName() {
-		return recipeName;
-	}
+    public String getRecipeName() {
+        return recipeName;
+    }
 
-	public void setRecipeName(String recipeName) {
-		this.recipeName = recipeName;
-	}
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
 
 }
